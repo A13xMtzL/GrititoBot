817cd72b1b1b98c8e2fa75bc5019f19308a56d35
Alejandro Martínez Luna <A01276785@tec.mx>
2024-01-16 21:45:37 +0100

    Refactor Discord bot code

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-const Discord = require("discord.js");
-// Trae las variables de entorno
-// const env = require("dotenv");
-const prefix = "<<";
+const { Client, Intents } = require("discord.js");
+const client = new Client({
 
-const bot = new Discord.Client({
-  description:
-    "Esto es un bot de prueba, aún no sabemos qué estamos haciendo xd\nCualquier duda, guárdesela",
-  intents: [
-    "GUILD_MESSAGES",
-    "GUILD_MESSAGE_REACTIONS",
-    "DIRECT_MESSAGES",
-    "DIRECT_MESSAGE_REACTIONS",
-  ],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
+const config = require("./config.json");
 
-bot.login(
-  "MTA1NDQ5NzgyNDE2NDgxNDk2MQ.GVYGgo.iDbf5lUviNZrWet5GVuE7FqG3USyMgnytxROjo"
-);
-
-bot.on("ready", () => {
-  console.log(`Logged in as ${bot.user.tag} and is ready! `);
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag} and is ready! `);
 });
-
-// // bot.on("message", (message) => {
+// // client.on("message", (message) => {
 // //   if (message.author.bot) return; // Evitar responder a otros bots o a sí mismo
 // //   console.log("Contenido del mensaje: " + message.content.toString());
 // //   message.channel.send("El mensaje que se mandó fue: " + message.content);
 // //   message.channel.send("I received your message!");
 // // });
 
-bot.on("message", (message) => {
-  if (!message.content.startsWith(prefix)) return;
+// client.on("message", (message) => {
+//   if (!message.content.startsWith(prefix)) return;
 
-  const command = message.content.slice(prefix.length).trim().split(' ')[0];
+//   const command = message.content.slice(prefix.length).trim().split(' ')[0];
 
-  if (command === "ping") {
+//   if (command === "ping") {
+//     const latency = Date.now() - message.createdTimestamp;
+//     message.channel.send(`**pong!** \t Latency: ${latency}ms`);
+//   }
+// });
+
+client.on("message", (message) => {
+
+  if (message.author.bot) return;
+  if (message.author.id !== config.ownerID) return;
+
+  if (message.content.startsWith(config.prefix + "ping")) {
     const latency = Date.now() - message.createdTimestamp;
-    message.channel.send(`**pong!** \t Latency: ${latency}ms`);
+    message.channel.send(`**Pong!** \t Latency: ${latency}ms`);
   }
 });
 
+
+
+// Secret token for the bot
+client.login(config.token);
